b2f01ca56190faa6ddac5157f3dc1b1e5c28a2e4
xcphoenix <root@xcphoenix.top>
2019-12-29 15:18:25 +0800

    feat(redis-config): 添加配置集群

--- src/main/resources/redis/sh/pre.sh
+++ src/main/resources/redis/sh/pre.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-source ./constant.sh 
+source ./constant.sh
+
+# create network
 docker network create --subnet=${REDIS_NETWORK_IP}/24 ${REDIS_NETWORK}
+
+# create rerdis-cluster
+# master:slave 1:1
+redis-cli --cluster create 169.69.2.2:7001 169.69.2.3:7002 169.69.2.4:7003 169.69.2.5:7004 169.69.2.6:7005 169.69.2.7:7006 --cluster-replicas 1
