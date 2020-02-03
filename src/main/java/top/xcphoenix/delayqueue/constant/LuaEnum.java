4568666cca313865a5691a408b27ce5a85c390d3
xcphoenix <root@xcphoenix.top>
2020-02-03 22:48:54 +0800

    feat(task): add task

---
+++ src/main/java/top/xcphoenix/delayqueue/constant/LuaEnum.java
@@ -0,0 +1,59 @@
+package top.xcphoenix.delayqueue.constant;
+
+import org.springframework.util.StreamUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+/**
+ * @author      xuanc
+ * @date        2020/2/3 下午12:01
+ * @version     1.0
+ */
+public enum LuaEnum {
+
+    /**
+     * 添加任务
+     */
+    ADD_TASK("addTask"),
+    /**
+     * 将任务推送至待消费队列
+     */
+    PUSH_TASK("pushTask"),
+    /**
+     * 移除未在待消费队列中的任务
+     */
+    REMOVE_TASK("removeTask");
+
+    LuaEnum(String fileName) {
+        this.fileName = fileName + ".lua";
+    }
+
+    private String fileName;
+    private static final String LOCATION;
+    static {
+        String res = "redis/lua";
+        URL url = LuaEnum.class.getClassLoader().getResource(res);
+        LOCATION = URLDecoder.decode(
+                        Objects.requireNonNull(url).getFile(),
+                        StandardCharsets.UTF_8
+                );
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getContent() throws IOException {
+        File file = new File(LOCATION, this.fileName);
+        return StreamUtils.copyToString(
+                new FileInputStream(file), StandardCharsets.UTF_8
+        );
+    }
+
+}
--- src/test/java/top/xcphoenix/delayqueue/LuaTest.java
+++ src/test/java/top/xcphoenix/delayqueue/LuaTest.java
@@ -1,15 +1,20 @@
 package top.xcphoenix.delayqueue;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.xcphoenix.delayqueue.service.DelayQueueService;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author      xuanc
  * @date        2020/1/31 下午9:48
  * @version     1.0
  */
+@Slf4j
 @SpringBootTest
 public class LuaTest {
 
@@ -17,8 +22,10 @@ public class LuaTest {
     private DelayQueueService delayQueueService;
 
     @Test
-    void testLoadScripts() throws Exception {
-        delayQueueService.init();
+    void testLoadScripts() throws IOException {
+        // log.info(LuaEnum.ADD_TASK.getContent());
+        File file = new File(String.valueOf(this.getClass().getClassLoader().getResource("./")));
+        log.info(file.getAbsolutePath());
     }
 
 
